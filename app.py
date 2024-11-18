dac9b42c2297afe78d91c3a0f17d038d6bd5438c
Mahesh <umamaheshbatraju@gmail.com>
2024-11-18 16:55:04 +0530

    Adding app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print("Welcome Hello World")
+
+print("Registration form completed")
+print("Login success")
+
+print("Login and Registration all success")
+print("This is my world to welcome")
