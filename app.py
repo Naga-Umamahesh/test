b2f8b738ecb496de52501eed0f0abeb79db578a1
Mahesh <umamaheshbatraju@gmail.com>
2024-11-18 17:04:50 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ print("Login success")
 print("Login and Registration all success")
 print("This is my world to welcome")
 print("This is my world you can come")
+if(n % 2 == 0):
+	print("Even number")
+else:
+	print("Odd number")
+
